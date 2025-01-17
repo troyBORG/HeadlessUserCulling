ccaff68fe3981bcf9b264a899081d9f8acb11cb8
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-01-16 17:35:10 -0800

    Initial mod setup

---
+++ HeadlessAvatarCulling/HeadlessAvatarCulling.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using ResoniteModLoader;
+using System;
+using System.Reflection;
+using FrooxEngine;
+using Elements.Core;
+
+namespace HeadlessAvatarCulling;
+
+public partial class HeadlessAvatarCulling : ResoniteMod
+{
+    public override string Name => "HeadlessAvatarCulling";
+    public override string Author => "Raidriar796";
+    public override string Version => "1.0.0";
+    public override string Link => "";
+    public static ModConfiguration? Config;
+
+    public override void OnEngineInit()
+    {
+        Harmony harmony = new("net.raidriar796.HeadlessAvatarCulling");
+        Config = GetConfiguration();
+        Config?.Save(true);
+        harmony.PatchAll();
+    }
+}
