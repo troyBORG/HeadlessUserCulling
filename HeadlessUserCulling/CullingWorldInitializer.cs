ded3047a66686597a9e9e4f1f91a9a02bdd570d0
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-02-05 17:23:04 -0800

    Changed distance var to world space
    I realized there wasn't a need to keep the system's variables to a scoped down dynamic variable space, so I've moved the variable to "World/", removed the dynvar space, and removed the slot variable.

--- HeadlessUserCulling/CullingWorldInitializer.cs
+++ HeadlessUserCulling/CullingWorldInitializer.cs
@@ -18,24 +18,12 @@ public partial class HeadlessUserCulling : ResoniteMod
                 Slot DynVarSlot = CullingRoot.AddSlot("DynVars", false);
                 CullingRoot.Tag = "HeadlessCullingRoot";
 
-                // Setup DynamicVariableSpace to allow for user configuration
-                var DynVarSpace = CullingRoot.AttachComponent<DynamicVariableSpace>();
-                DynVarSpace.SpaceName.Value = "HeadlessUserCulling";
-
-                // Setup HeadlessCullingRoot variable
-                Slot RootVarSlot = DynVarSlot.AddSlot("HeadlessCullingRoot", false);
-
-                var RootDynVar = RootVarSlot.AttachComponent<DynamicReferenceVariable<Slot>>();
-                RootDynVar.VariableName.Value = "World/HeadlessCullingRoot";
-                RootDynVar.Reference.Target = CullingRoot;
-                RootDynVar.Reference.DriveFrom(RootDynVar.Reference);
-
                 // Setup CullingDistance variable
                 Slot DistanceVarSlot = DynVarSlot.AddSlot("CullingDistance", false);
                 DistanceVarSlot.Tag = "DistanceVar";
 
                 var DistanceDynVar = DistanceVarSlot.AttachComponent<DynamicValueVariable<float>>();
-                DistanceDynVar.VariableName.Value = "HeadlessUserCulling/CullingDistance";
+                DistanceDynVar.VariableName.Value = "World/CullingDistance";
 
                 var DistanceOverride = DistanceVarSlot.AttachComponent<ValueUserOverride<float>>();
                 DistanceOverride.Default.Value = 10;
