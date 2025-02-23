f7433d257859af85f3001413a6615f3886d177f8
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-02-22 23:58:40 -0800

    Restructure context menu
    The context menu is now a sub menu that leads to the distance setting and another sub menu to populate mute buttons within.

--- HeadlessUserCulling/ContextMenuInitializer.cs
+++ HeadlessUserCulling/ContextMenuInitializer.cs
@@ -11,42 +11,63 @@ public partial class HeadlessUserCulling : ResoniteMod
         Slot ThisUserRoot = user.Root.Slot;
 
         // Sets up context menu
-        Slot ContextMenuSlot = ThisUserRoot.AddSlot("HeadlessCullingContextMenu", false);
+        Slot RootMenuSlot = ThisUserRoot.AddSlot("HeadlessCullingContextMenu", false);
+        Slot DistanceMenuSlot = RootMenuSlot.AddSlot("CullingDistance", false);
+        Slot MuteMenuSlot = RootMenuSlot.AddSlot("MuteSettings", false);
+
         Slot DistVarSlot = CullingRoot.GetChildrenWithTag("DistanceVar").First();
         if (!DistVarSlot.IsDestroyed)
         {
             var DistVar = DistVarSlot.GetComponent<DynamicValueVariable<float>>();
 
-            var ItemSource = ContextMenuSlot.AttachComponent<ContextMenuItemSource>();
-            ItemSource.Color.Value = colorX.Yellow;
+            // Root Context Menu Button
+            var RootItemSource = RootMenuSlot.AttachComponent<ContextMenuItemSource>();
+            RootItemSource.Label.Value = "User Culling Settings";
+            RootItemSource.Color.Value = colorX.Cyan;
 
-            var StringDriver = ContextMenuSlot.AttachComponent<MultiValueTextFormatDriver>();
+            var RootItem = RootMenuSlot.AttachComponent<RootContextMenuItem>();
+            RootItem.ExcludeOnTools.Value = true;
+            RootItem.Item.Target = RootItemSource;
+
+            var RootSubmenu = RootMenuSlot.AttachComponent<ContextMenuSubmenu>();
+            RootSubmenu.ItemsRoot.Target = RootMenuSlot;
+
+            // Culling Distance Button
+            var DistanceItemSource = DistanceMenuSlot.AttachComponent<ContextMenuItemSource>();
+            DistanceItemSource.Color.Value = colorX.Yellow;
+
+            var StringDriver = DistanceMenuSlot.AttachComponent<MultiValueTextFormatDriver>();
             StringDriver.Sources.Add(DistVar.Value);
             StringDriver.Format.Value = "Culling Distance: {0}";
-            StringDriver.Text.Target = ItemSource.Label;
+            StringDriver.Text.Target = DistanceItemSource.Label;
 
-            var RootItem = ContextMenuSlot.AttachComponent<RootContextMenuItem>();
-            RootItem.ExcludeOnTools.Value = true;
-            RootItem.Item.Target = ItemSource;
-
-            var ButtonCycle = ContextMenuSlot.AttachComponent<ButtonValueCycle<float>>();
+            var ButtonCycle = DistanceMenuSlot.AttachComponent<ButtonValueCycle<float>>();
             ButtonCycle.TargetValue.Target = DistVar.Value;
             ButtonCycle.Values.Add(float.PositiveInfinity);
             ButtonCycle.Values.Add(20F);
             ButtonCycle.Values.Add(10F);
             ButtonCycle.Values.Add(5F);
             ButtonCycle.Values.Add(2F);
+
+            // Mute Settings Submenu
+            var MuteItemSource = MuteMenuSlot.AttachComponent<ContextMenuItemSource>();
+            MuteItemSource.Label.Value = "Mute Settings";
+            MuteItemSource.Color.Value = colorX.Red;
+
+            var MuteSubmenu = MuteMenuSlot.AttachComponent<ContextMenuSubmenu>();
+            MuteSubmenu.ItemsRoot.Target = RootMenuSlot;
+            MuteSubmenu.Hidden.Value = true;
         }
 
         // Sets up the context menu to destroy itself when
         // the user's culling root is destroyed
         UserCullingSlot.Destroyed += d =>
         {
-            if (!user.IsDestroyed && !ThisUserRoot.IsDestroyed && !ContextMenuSlot.IsDestroyed) ContextMenuSlot.Destroy();
+            if (!user.IsDestroyed && !ThisUserRoot.IsDestroyed && !RootMenuSlot.IsDestroyed) RootMenuSlot.Destroy();
         };
 
         // Regenerates the context menu if it's directly destroyed
-        ContextMenuSlot.Destroyed += d =>
+        RootMenuSlot.Destroyed += d =>
         {
             if (!user.IsDestroyed && !CullingRoot.IsDestroyed && !UserCullingSlot.IsDestroyed && !ThisUserRoot.IsDestroyed)
             {
